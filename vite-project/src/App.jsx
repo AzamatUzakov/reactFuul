1d0cb700a313b862d06760257211accacadede6f
AzamatUzakov <azamatakmal053@gmail.com>
2025-02-25 12:36:56 +0500

    props практическая работа с маисвами

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,28 +1,28 @@
 import { useState } from 'react'
 import './App.css'
-const users = [
-  { id: 1, name: "Иван", age: 25 },
-  { id: 2, name: "Мария", age: 28 },
-  { id: 3, name: "Петр", age: 35 }
-];
 
 
 
 function App() {
-
+  
+  const users = [
+    { id: 1, name: "Иван", age: 25 },
+    { id: 2, name: "Мария", age: 28 },
+    { id: 3, name: "Петр", age: 35 }
+  ];
   return (
     <div>
-      <UserList />
+      <UserList users={users} />
     </div>
   );
 }
 
 
-function UserList() {
+function UserList({users}) {
   return (
     <>
       {
-        users.map((user, idx) => (
+        users.map((user) => (
           <div className="list" key={user.id}>
             <h1>{user.name}</h1>
             <p>{user.age}</p>
