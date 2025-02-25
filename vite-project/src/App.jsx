126683b0babf3ba2a8a81130caec4e971cf843bf
AzamatUzakov <azamatakmal053@gmail.com>
2025-02-25 15:01:10 +0500

    props практическая работа с маисвами

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,32 +4,13 @@ import './App.css'
 
 
 function App() {
-  
-  const users = [
-    { id: 1, name: "Иван", age: 25 },
-    { id: 2, name: "Мария", age: 28 },
-    { id: 3, name: "Петр", age: 35 }
-  ];
+
   return (
     <div>
-      <UserList users={users} />
+   
+    
     </div>
   );
 }
 
-
-function UserList({users}) {
-  return (
-    <>
-      {
-        users.map((user) => (
-          <div className="list" key={user.id}>
-            <h1>{user.name}</h1>
-            <p>{user.age}</p>
-          </div>
-        ))
-      }
-    </>
-  )
-}
 export default App
