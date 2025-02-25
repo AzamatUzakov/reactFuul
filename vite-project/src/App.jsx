ab9c7df0a564d6497334e9a62b1c946a79a9680e
AzamatUzakov <azamatakmal053@gmail.com>
2025-02-25 16:13:58 +0500

    функция включено выключено

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,11 +4,17 @@ import './App.css'
 
 
 function App() {
-
+  const [onSelected, setOnSelected] = useState(true)
+  const on = () => {
+    setOnSelected(prev => !prev)
+  }
   return (
+
     <div>
-   
-    
+
+
+      <button onClick={() => on()}>{onSelected ? "Включено" : "Выключено"}</button>
+
     </div>
   );
 }
