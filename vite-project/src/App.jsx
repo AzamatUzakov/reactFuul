b0c2a546d5abb61bb566ccb968f6e7b6a0a8844f
AzamatUzakov <azamatakmal053@gmail.com>
2025-02-28 14:22:59 +0500

    useEffect

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
 
+function Example() {
+  const [count, setCount] = useState(0);
 
-
-function App() {
-  const [count, setCount] = useState(0)
-  const [step, setStep] = useState(1)
-
-  const plusBtn = () => {
-    setCount(prev => prev + step)
-  }
-  const minBtn = () => {
-    setCount(prev => prev - step)
-  }
+  useEffect(() => {
+    console.log("Компонент обновился!");
+  });
 
   return (
     <div>
-      <button onClick={() => plusBtn()}>+</button> <br />
-      <h1>{count}</h1>
-      <input type="number" value={step} onChange={(e)=>setStep(Number(e.target.value))} /><br />
-      <button onClick={() => minBtn()}>-</button>
+      <p>Счетчик: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
 
-export default App
+export default Example;
